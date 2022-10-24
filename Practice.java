a8cf1fc0a5f8b4f9b25fc81972d785afb0bf6160
11645 <ian.rovner@vtsd.com>
2022-10-24 07:38:13 -0400

    Daily Challenge

---
+++ Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+        public static void main(String args[]){
+
+
+             for(int n=100 ; n >= 0; n-=3){
+
+
+
+
+                 System.out.print(n + ",");
+             }
+
+
+
+        }
+}
